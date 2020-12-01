3c50b72eacd846054f1d3a4224fb2500fe670af8
pabichw <pabichwiktor@gmail.com>
2020-12-01 23:34:48 +0100

    404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Layout from '../src/components/layout';
+import styled from 'styled-components';
 
 export default () => (
   <Layout>
-    <div>
-      <h1>Oops, that's a 404</h1>
-    </div>
+    <Container>
+      <Content>
+        <h1>O nie, ta strona nie istnieje!</h1>
+        <h4>Sprawdz czy adres jest poprawny</h4>
+      </Content>
+    </Container>
   </Layout>
 )
+
+
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 4em;
+  min-height: calc(100vh - 85px);
+`
+
+const Content = styled.div`
+  text-align: center;
+`
